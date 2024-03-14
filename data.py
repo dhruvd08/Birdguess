adee3b3e1b3d1183ce7839cddac09a516d5b261e
Maulik Dave <mauldave@gmail.com>
2024-03-14 21:36:40 +0530

    Initial commit

---
+++ data.py
@@ -0,0 +1,26 @@
+birds = {
+    'black winged kite': {
+        'countries': ['IN'],
+        'img': 'images/black-winged-kite.jpg'
+    },
+
+    'coppersmith barbet': {
+        'countries': ['IN'],
+        'img': 'images/coppersmith-barbet.jpg'
+    },
+
+    'greater coucal': {
+        'countries': ['IN'],
+        'img': 'images/greater-coucal.jpg'
+    },
+
+    'malabar pied hornbill': {
+        'countries': ['IN'],
+        'img': 'images/malabar-pied-hornbill.jpg'
+    },
+
+    'orange minivet': {
+        'countries': ['IN'],
+        'img': 'images/orange-minivet.jpg'
+    }
+}
---
+++ main.py
@@ -0,0 +1,39 @@
+import http
+import time
+
+import requests
+
+from data import birds
+from flask import Flask, request
+
+app = Flask(__name__)
+
+
+def send(msg_type: str, msg: str, to: int):
+    endpoint = 'https://foresthut.pythonanywhere.com/messages/message'
+
+    input_params = {
+        'msg_id': str(int(time.time())),
+        'msg_body': msg,
+        'msg_type': msg_type,
+        'player_id': to
+    }
+
+    header = {
+        'api-key': 'fa64f9edd0351f4238d7cbfa5b8e1c12e148aa1629bdceefe639bee8b93a2d5d'
+    }
+
+    response = requests.post(url=endpoint, json=input_params, headers=header).json()
+    print(response)
+
+
+@app.route('/notify-wh', methods=['POST'])
+def notify():
+    content = request.json
+    print(content)
+    # TODO: do process the message
+    return str(http.HTTPStatus.OK.value)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
