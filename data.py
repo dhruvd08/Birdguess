de5c2be4cffd8b1a19c6f0dabd564a20ec37ff3d
Maulik Dave <mauldave@gmail.com>
2024-03-23 14:51:50 +0530

    add 35 more birds and uncomment them in "data.py"

--- data.py
+++ data.py
@@ -158,176 +158,181 @@ species = {
         'countries': ['IN'],
         'img': 'images/brahminy-kite.jpg'
     },
-    #
-    # 'indian vulture': {
-    #     'countries': ['IN'],
-    #     'img': 'images/indian-vulture.jpg'
-    # },
-    #
-    # 'white rumped vulture': {
-    #     'countries': ['IN'],
-    #     'img': 'images/white-rumped-vulture.jpg'
-    # },
-    #
-    # 'jungle nightjar': {
-    #     'countries': ['IN'],
-    #     'img': 'images/jungle-nightjar.jpg'
-    # },
-    #
-    # 'indian eagle owl': {
-    #     'countries': ['IN'],
-    #     'img': 'images/indian-eagle-owl.jpg'
-    # },
-    #
-    # 'brown wood owl': {
-    #     'countries': ['IN'],
-    #     'img': 'images/brown-wood-owl.jpg'
-    # },
-    #
-    # 'white throated kingfisher': {
-    #     'countries': ['IN'],
-    #     'img': 'images/white-throated-kingfisher.jpg'
-    # },
-    #
-    # 'common kingfisher': {
-    #     'countries': ['IN'],
-    #     'img': 'images/common-kingfisher.jpg'
-    # },
-    #
-    # 'indian roller': {
-    #     'countries': ['IN'],
-    #     'img': 'images/indian-roller.jpg'
-    # },
-    #
-    # 'asian green beeeater': {
-    #     'countries': ['IN'],
-    #     'img': 'images/asian-green-beeeater.jpg'
-    # },
-    #
-    # 'common kestrel': {
-    #     'countries': ['IN'],
-    #     'img': 'images/common-kestrel.jpg'
-    # },
-    #
-    # 'indian robin': {
-    #     'countries': ['IN'],
-    #     'img': 'images/indian-robin.jpg'
-    # },
-    #
-    # 'red breasted flycatcher': {
-    #     'countries': ['IN'],
-    #     'img': 'images/red-breasted-flycatcher.jpg'
-    # },
-    #
-    # 'common woodshrike': {
-    #     'countries': ['IN'],
-    #     'img': 'images/common-woodshrike.jpg'
-    # },
-    #
-    # 'jerdons leafbird': {
-    #     'countries': ['IN'],
-    #     'img': 'images/jerdons-leafbird.jpg'
-    # },
-    #
-    # 'barn owl': {
-    #     'countries': ['IN'],
-    #     'img': 'images/barn-owl.jpg'
-    # },
-    #
-    # 'purple sunbird': {
-    #     'countries': ['IN'],
-    #     'img': 'images/purple-sunbird.jpg'
-    # },
-    #
-    # 'purple rumped sunbird': {
-    #     'countries': ['IN'],
-    #     'img': 'images/purple-rumped-sunbird.jpg'
-    # },
-    #
-    # 'crimson backed sunbird': {
-    #     'countries': ['IN'],
-    #     'img': 'images/crimson-backed-sunbird.jpg'
-    # },
-    #
-    # 'puff throated babbler': {
-    #     'countries': ['IN'],
-    #     'img': 'images/puff-throated-babbler.jpg'
-    # },
-    #
-    # 'jungle babbler': {
-    #     'countries': ['IN'],
-    #     'img': 'images/jungle-babbler.jpg'
-    # },
-    #
-    # 'indian pond heron': {
-    #     'countries': ['IN'],
-    #     'img': 'images/indian-pond-heron.jpg'
-    # },
-    #
-    # 'grey heron': {
-    #     'countries': ['IN'],
-    #     'img': 'images/grey-heron.jpg'
-    # },
-    #
-    # 'black crowned night heron': {
-    #     'countries': ['IN'],
-    #     'img': 'images/black-crowned-night-heron.jpg'
-    # },
-    #
-    # 'common myna': {
-    #     'countries': ['IN'],
-    #     'img': 'images/common-myna.jpg'
-    # },
-    #
-    # 'common hawk cuckoo': {
-    #     'countries': ['IN'],
-    #     'img': 'images/common-hawk-cuckoo.jpg'
-    # },
-    #
-    # 'great hornbill': {
-    #     'countries': ['IN'],
-    #     'img': 'images/great-hornbill.jpg'
-    # },
-    #
-    # 'red wattled lapwing': {
-    #     'countries': ['IN'],
-    #     'img': 'images/red-wattled-lapwing.jpg'
-    # },
-    #
-    # 'white breasted waterhen': {
-    #     'countries': ['IN'],
-    #     'img': 'images/white-breasted-waterhen.jpg'
-    # },
-    #
-    # 'indian paradise flycatcher': {
-    #     'countries': ['IN'],
-    #     'img': 'images/indian-paradise-flycatcher.jpg'
-    # },
-    #
-    # 'little cormorant': {
-    #     'countries': ['IN'],
-    #     'img': 'images/little-cormorant.jpg'
-    # },
-    #
-    # 'orange headed thrush': {
-    #     'countries': ['IN'],
-    #     'img': 'images/orange-headed-thrush.jpg'
-    # },
-    #
-    # 'malabar whistling thrush': {
-    #     'countries': ['IN'],
-    #     'img': 'images/malabar-whistling-thrush.jpg'
-    # },
-    #
-    # 'rose ringed parakeet': {
-    #     'countries': ['IN'],
-    #     'img': 'images/rose-ringed-parakeet.jpg'
-    # },
-    #
-    # 'plum headed parakeet': {
-    #     'countries': ['IN'],
-    #     'img': 'images/plum-headed-parakeet.jpg'
-    # },
+
+    'indian vulture': {
+        'countries': ['IN'],
+        'img': 'images/indian-vulture.jpg'
+    },
+
+    'malabar trogon': {
+        'countries': ['IN'],
+        'img': 'images/malabar-trogon.jpg'
+    },
+
+    'white rumped vulture': {
+        'countries': ['IN'],
+        'img': 'images/white-rumped-vulture.jpg'
+    },
+
+    'jungle nightjar': {
+        'countries': ['IN'],
+        'img': 'images/jungle-nightjar.jpg'
+    },
+
+    'indian eagle owl': {
+        'countries': ['IN'],
+        'img': 'images/indian-eagle-owl.jpg'
+    },
+
+    'brown wood owl': {
+        'countries': ['IN'],
+        'img': 'images/brown-wood-owl.jpg'
+    },
+
+    'white throated kingfisher': {
+        'countries': ['IN'],
+        'img': 'images/white-throated-kingfisher.jpg'
+    },
+
+    'common kingfisher': {
+        'countries': ['IN'],
+        'img': 'images/common-kingfisher.jpg'
+    },
+
+    'indian roller': {
+        'countries': ['IN'],
+        'img': 'images/indian-roller.jpg'
+    },
+
+    'asian green beeeater': {
+        'countries': ['IN'],
+        'img': 'images/asian-green-beeeater.jpg'
+    },
+
+    'common kestrel': {
+        'countries': ['IN'],
+        'img': 'images/common-kestrel.jpg'
+    },
+
+    'indian robin': {
+        'countries': ['IN'],
+        'img': 'images/indian-robin.jpg'
+    },
+
+    'red breasted flycatcher': {
+        'countries': ['IN'],
+        'img': 'images/red-breasted-flycatcher.jpg'
+    },
+
+    'common woodshrike': {
+        'countries': ['IN'],
+        'img': 'images/common-woodshrike.jpg'
+    },
+
+    'jerdons leafbird': {
+        'countries': ['IN'],
+        'img': 'images/jerdons-leafbird.jpg'
+    },
+
+    'barn owl': {
+        'countries': ['IN'],
+        'img': 'images/barn-owl.jpg'
+    },
+
+    'purple sunbird': {
+        'countries': ['IN'],
+        'img': 'images/purple-sunbird.jpg'
+    },
+
+    'purple rumped sunbird': {
+        'countries': ['IN'],
+        'img': 'images/purple-rumped-sunbird.jpg'
+    },
+
+    'crimson backed sunbird': {
+        'countries': ['IN'],
+        'img': 'images/crimson-backed-sunbird.jpg'
+    },
+
+    'puff throated babbler': {
+        'countries': ['IN'],
+        'img': 'images/puff-throated-babbler.jpg'
+    },
+
+    'jungle babbler': {
+        'countries': ['IN'],
+        'img': 'images/jungle-babbler.jpg'
+    },
+
+    'indian pond heron': {
+        'countries': ['IN'],
+        'img': 'images/indian-pond-heron.jpg'
+    },
+
+    'grey heron': {
+        'countries': ['IN'],
+        'img': 'images/grey-heron.jpg'
+    },
+
+    'black crowned night heron': {
+        'countries': ['IN'],
+        'img': 'images/black-crowned-night-heron.jpg'
+    },
+
+    'common myna': {
+        'countries': ['IN'],
+        'img': 'images/common-myna.jpg'
+    },
+
+    'common hawk cuckoo': {
+        'countries': ['IN'],
+        'img': 'images/common-hawk-cuckoo.jpg'
+    },
+
+    'great hornbill': {
+        'countries': ['IN'],
+        'img': 'images/great-hornbill.jpg'
+    },
+
+    'red wattled lapwing': {
+        'countries': ['IN'],
+        'img': 'images/red-wattled-lapwing.jpg'
+    },
+
+    'white breasted waterhen': {
+        'countries': ['IN'],
+        'img': 'images/white-breasted-waterhen.jpg'
+    },
+
+    'indian paradise flycatcher': {
+        'countries': ['IN'],
+        'img': 'images/indian-paradise-flycatcher.jpg'
+    },
+
+    'little cormorant': {
+        'countries': ['IN'],
+        'img': 'images/little-cormorant.jpg'
+    },
+
+    'orange headed thrush': {
+        'countries': ['IN'],
+        'img': 'images/orange-headed-thrush.jpg'
+    },
+
+    'malabar whistling thrush': {
+        'countries': ['IN'],
+        'img': 'images/malabar-whistling-thrush.jpg'
+    },
+
+    'rose ringed parakeet': {
+        'countries': ['IN'],
+        'img': 'images/rose-ringed-parakeet.jpg'
+    },
+
+    'plum headed parakeet': {
+        'countries': ['IN'],
+        'img': 'images/plum-headed-parakeet.jpg'
+    },
     #
     # 'clamorous reed warbler': {
     #     'countries': ['IN'],
